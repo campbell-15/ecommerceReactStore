d2b0341f7f13692675eb471b089988b5d6d54b39
Rethabileenno <rethabileenno@gmail.com>
2024-04-18 13:47:12 +0200

    Completed the left dashboard, product structure and search bar

---
+++ src/dashboard.jsx
@@ -0,0 +1,234 @@
+import React from 'react';
+import './dashboard.css';
+
+import logo from './Assets/Logo.png'
+import menue from './Assets/Menu-Icon.png'
+import home from './Assets/Nav-Link.png'
+import bag from './Assets/bag.png'
+import logout from './Assets/rednextbutton.png'
+
+function Dashboard() {
+
+  return (
+    <div className='Body'> 
+
+
+                        <div className="Navbar">
+                        <div className="NavLinksTop">
+
+                            <div className="Logo">
+                                <img  src={logo} alt=''/>
+                            </div>
+
+                            <div className="MenuIcon">
+                                <img  src={menue} alt=''/>
+                            </div>
+
+                            <div className="NavLink">
+                            <img  src={home} alt=''/>
+                            </div>
+
+
+                            <img  src={bag} alt=''/>
+                              
+                        </div>
+
+
+                        <div className="NavLinkBottom">
+                            <img  src={logout} alt=''/>
+                        </div>
+
+                        </div>
+                    <div className="TopBar">
+                        <div className="Input">
+                        <div className="Label">
+                            <div className="Label">Search Item</div>
+                        </div>
+                        <div className="InputField">
+                            <div className="InputArea">
+                            <div className="Placeholder">Apple Watch, Samsung S21, Macbook Pro, ...</div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+
+                    <div className="Container">
+
+                 <div className="ItemCard">
+                    <div className="ProductImage">
+                        <div className="ProductImageInner">
+                        <img className="Image" src="https://via.placeholder.com/159x200" />
+                        </div>
+                    </div>
+                    <div className="Content">
+                        <div className="ItemName">Apple Watch</div>
+                        <div className="SmallDescription">Series 5 SE</div>
+                        <div className="Action">
+                        <div className="Price">$ 529.99</div>
+                        <div className="Button">
+                            <div className="Icon">
+                            <div className="Vector"></div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    </div> 
+
+                    <div className="ItemCard">
+                    <div className="ProductImage">
+                        <div className="ProductImageInner">
+                        <img className="Image" src="https://via.placeholder.com/159x200" />
+                        </div>
+                    </div>
+                    <div className="Content">
+                        <div className="ItemName">Apple Watch</div>
+                        <div className="SmallDescription">Series 5 SE</div>
+                        <div className="Action">
+                        <div className="Price">$ 529.99</div>
+                        <div className="Button">
+                            <div className="Icon">
+                            <div className="Vector"></div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    </div> 
+                    <div className="ItemCard">
+                    <div className="ProductImage">
+                        <div className="ProductImageInner">
+                        <img className="Image" src="https://via.placeholder.com/159x200" />
+                        </div>
+                    </div>
+                    <div className="Content">
+                        <div className="ItemName">Apple Watch</div>
+                        <div className="SmallDescription">Series 5 SE</div>
+                        <div className="Action">
+                        <div className="Price">$ 529.99</div>
+                        <div className="Button">
+                            <div className="Icon">
+                            <div className="Vector"></div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    </div> 
+                    <div className="ItemCard">
+                    <div className="ProductImage">
+                        <div className="ProductImageInner">
+                        <img className="Image" src="https://via.placeholder.com/159x200" />
+                        </div>
+                    </div>
+                    <div className="Content">
+                        <div className="ItemName">Apple Watch</div>
+                        <div className="SmallDescription">Series 5 SE</div>
+                        <div className="Action">
+                        <div className="Price">$ 529.99</div>
+                        <div className="Button">
+                            <div className="Icon">
+                            <div className="Vector"></div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    </div> 
+                    <div className="ItemCard">
+                    <div className="ProductImage">
+                        <div className="ProductImageInner">
+                        <img className="Image" src="https://via.placeholder.com/159x200" />
+                        </div>
+                    </div>
+                    <div className="Content">
+                        <div className="ItemName">Apple Watch</div>
+                        <div className="SmallDescription">Series 5 SE</div>
+                        <div className="Action">
+                        <div className="Price">$ 529.99</div>
+                        <div className="Button">
+                            <div className="Icon">
+                            <div className="Vector"></div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    </div> 
+                    <div className="ItemCard">
+                    <div className="ProductImage">
+                        <div className="ProductImageInner">
+                        <img className="Image" src="https://via.placeholder.com/159x200" />
+                        </div>
+                    </div>
+                    <div className="Content">
+                        <div className="ItemName">Apple Watch</div>
+                        <div className="SmallDescription">Series 5 SE</div>
+                        <div className="Action">
+                        <div className="Price">$ 529.99</div>
+                        <div className="Button">
+                            <div className="Icon">
+                            <div className="Vector"></div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    </div> 
+                    <div className="ItemCard">
+                    <div className="ProductImage">
+                        <div className="ProductImageInner">
+                        <img className="Image" src="https://via.placeholder.com/159x200" />
+                        </div>
+                    </div>
+                    <div className="Content">
+                        <div className="ItemName">Apple Watch</div>
+                        <div className="SmallDescription">Series 5 SE</div>
+                        <div className="Action">
+                        <div className="Price">$ 529.99</div>
+                        <div className="Button">
+                            <div className="Icon">
+                            <div className="Vector"></div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    </div> 
+                    <div className="ItemCard">
+                    <div className="ProductImage">
+                        <div className="ProductImageInner">
+                        <img className="Image" src="https://via.placeholder.com/159x200" />
+                        </div>
+                    </div>
+                    <div className="Content">
+                        <div className="ItemName">Apple Watch</div>
+                        <div className="SmallDescription">Series 5 SE</div>
+                        <div className="Action">
+                        <div className="Price">$ 529.99</div>
+                        <div className="Button">
+                            <div className="Icon">
+                            <div className="Vector"></div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    </div> 
+                    
+
+                    </div>
+                    
+                    
+                    {/* </div> */}
+                     {/* </div> */}
+                   
+                                
+{/*                       
+
+                        <div className="ContentArea">
+                       
+                       
+                    </div> */}
+
+
+                       
+       
+
+    </div>
+  );
+}
+
+export default Dashboard;
