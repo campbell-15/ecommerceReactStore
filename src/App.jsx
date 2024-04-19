c7523e8324b39205a6ffabd172dd9d2931bef8c7
Campbell Davies-Webb <campbelldavieswebb@gmail.com>
2024-04-19 12:02:10 +0200

    adding routing for AddPayment component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import CheckoutBagPage from './Components/CheckoutBagPage/CheckoutBagPage';
+import AddPayment from './Components/AddPayment/AddPayment';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 
@@ -8,6 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element={<CheckoutBagPage/>} />
+        <Route path='/change-payment' element={<AddPayment/>} />
       </Routes>
     </Router>
   );
