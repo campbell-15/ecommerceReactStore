8fba57859034794d617f73216396aaad21257494
Rethabileenno <rethabileenno@gmail.com>
2024-04-21 18:47:51 +0200

    All updates

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,14 @@ import AddPayment from './Components/AddPayment/AddPayment';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 
+import Dashboard  from './dashboard';
+
+
 function App() {
   return (
     <Router>
       <Routes>
+      <Route path="/" element={<Dashboard />} />
         <Route path='/view-bag' element={<CheckoutBagPage/>} />
         <Route path='/change-payment' element={<AddPayment/>} />
       </Routes>
