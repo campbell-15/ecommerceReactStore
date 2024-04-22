626fdff08d076c6c180fda13bb74b3d654890ee9
Campbell Davies-Webb <campbelldavieswebb@gmail.com>
2024-04-22 20:24:38 +0200

    comments for new page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import CheckoutBagPage from './Components/CheckoutBagPage/CheckoutBagPage';
 import AddPayment from './Components/AddPayment/AddPayment';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+// import Checkout from './Checkout';
+// import Items from './Items';
 
 import Dashboard  from './dashboard';
 import AddAddress from './AddAddress';
@@ -15,6 +17,8 @@ function App() {
         <Route path='/view-bag' element={<CheckoutBagPage/>} />
         <Route path='/change-payment' element={<AddPayment/>} />
         <Route path='/add-address' element={<AddAddress/>} />
+        {/* <Route path='/checkout' element={<Checkout/>} />
+        <Route path='/item-view' element={<Items/>} /> */}
       </Routes>
     </Router>
   );
