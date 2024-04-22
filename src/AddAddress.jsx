ffeb555c0e9e564c4f09e564092ae287b9458c9c
Rethabileenno <rethabileenno@gmail.com>
2024-04-22 17:27:41 +0200

    address page update

---
+++ src/AddAddress.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './AddAddress.css'
+// import { green } from '@mui/material/colors';
+import greenbag from './Assets/greenlock.png'
+
+const AddAddress = () => {
+   
+    return (
+        <div className="add-address-container">
+            
+
+        </div>
+        )
+    }
+    
+    export default AddAddress
